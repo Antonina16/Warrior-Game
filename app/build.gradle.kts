bd221c3f967956bac31d0c1ba00f4f33d7a031dd
aLehka <antonina.olegovna.lehka@gmail.com>
2025-10-13 20:21:39 +0300

    [Koin] added koin annotations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation (libs.androidx.ui.text.google.fonts.vcompose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
